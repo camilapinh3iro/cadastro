5bc7978802d5fa1b3685ec1a7d3d94d984589fa1
HasegawaTaizou <caiocoghi@gmail.com>
2023-04-20 08:01:42 -0300

    versao estavel 36.0

--- teste.php
+++ teste.php
@@ -8,3 +8,24 @@ $data = $data->add($interval)->format('Y-m-d');
 echo $data;
 
 //----------------------
+
+function gravar($user, $password, $expirationDate) {
+    $arquivo = "teste.txt";
+
+    $texto = "";
+    $texto .= "\n\nUsuário - $user\n";
+    $texto .= "Senha - $password\n";
+    $texto .= "Data de Expiração - $expirationDate\n\n";
+    $texto .= "--------------------------";
+
+
+    $fp = fopen($arquivo, "a+");
+
+    fwrite($fp, $texto);
+
+    fclose($fp);
+}
+
+gravar("joao", "123", "2022-04-09");
+
+?>
